ea5ad17a9b2ba2096509e0c3696e2769c94c23cb
Adam <Adam@Adam-Laptop>
2013-03-21 18:41:11 +0100

    wykres + filechooser

---
+++ src/firstTask/Diagram.java
@@ -0,0 +1,115 @@
+package firstTask;
+
+import java.awt.*;
+import java.io.File;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+class Diagram extends JPanel {
+   
+   int xLenght;
+   int[] y;
+   int maxHeight;
+   int width;
+   double hstep = 1.0;
+   File file;
+   
+   boolean once = false;
+   
+  public Diagram(int maxHeight, int width) {
+	super();
+	this.maxHeight = maxHeight;
+	this.width = width;
+}
+
+public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    
+    if(!once)
+    {
+    generateWaves();
+    once = true;
+    }
+    
+    int maxWidth = getWidth();
+    hstep = maxWidth / getWidth();
+    g.setColor(Color.BLUE);
+    for (int i = 1; i < xLenght; i++) {
+      int x1 = (int) ((i - 1) * hstep);
+      int x2 = (int) (i * hstep);
+      int y1 = y[i - 1];
+      int y2 = y[i];
+      g.drawLine(x1, y1, x2, y2);
+    }
+    g.setColor(Color.black);
+    g.drawLine(0, getHeight()/2, getWidth(), getHeight()/2);
+  }
+  
+  public void generateWaves()
+  {
+		try{
+		// Open the wav file specified as the first argument
+      WavFile wavFile = WavFile.openWavFile(file);
+
+      // Display information about the wav file
+//      wavFile.display();
+
+      // Get the number of audio channels in the wav file
+      int numChannels = wavFile.getNumChannels();
+      xLenght = (int)wavFile.getNumFrames();
+//      xLenght = 100;
+      y = new int[xLenght];
+      System.out.println(getHeight());
+      this.setSize(y.length, getHeight());
+      // Create a buffer of 100 frames
+      double[] buffer = new double[100 * numChannels];
+
+      int framesRead;
+//      double min = Double.MAX_VALUE;
+//      double max = Double.MIN_VALUE;
+      
+//      double hstep = (double) maxWidth / (double) points;
+      int maxHeight = getHeight();
+      System.out.println(maxHeight);
+      int i = 0;
+      do
+      {
+    	  
+         // Read frames into buffer
+         framesRead = wavFile.readFrames(buffer, 100);
+
+         // Loop through frames and look for minimum and maximum value
+         for (int s=0 ; s<framesRead * numChannels ; s++)
+         {
+        	 try
+        	 {
+        	y[100*i+s]=(int) (buffer[s] * maxHeight / 2 * 0.95 + maxHeight / 2);
+//            if (buffer[s] > max) max = buffer[s];
+//            if (buffer[s] < min) min = buffer[s];
+        	 }
+        	 catch(Exception e)
+        	 {
+        		 e.printStackTrace();
+        		 System.out.println("I: " + i + " S: "+ s + "\n" + (i*100+s));
+        	 }
+         }
+         i++;
+//         break;
+         
+      }
+      while (framesRead != 0);
+
+      // Close the wavFile
+      wavFile.close();
+
+      // Output the minimum and maximum value
+//      System.out.printf("Min: %f, Max: %f\n", min, max);
+   }
+   catch (Exception e)
+   {
+      System.err.println(e);
+   }
+
+  }
+}
--- src/firstTask/MainWindow.java
+++ src/firstTask/MainWindow.java
@@ -1,25 +1,37 @@
 package firstTask;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JScrollPane;
-import java.awt.Rectangle;
-import javax.swing.ScrollPaneConstants;
-import java.awt.Dimension;
 import java.awt.Color;
-import java.awt.FlowLayout;
-import javax.swing.BoxLayout;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class MainWindow extends JFrame {
 
 	private JPanel contentPane;
 	private JPanel panel;
 	private JScrollPane scrollPane;
-
+	private JTextField textFieldInputFile;
+	private Diagram diagram;
+	private JFileChooser fc = new JFileChooser();
+	private JButton btnGenerateWave;;
 
 	/**
 	 * Launch the application.
@@ -41,36 +53,152 @@ public class MainWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public MainWindow() {
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 472, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 424, 0 };
+		gbl_contentPane.rowHeights = new int[] { 200, 109, 0 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
+
+		diagram = new Diagram(100, getWidth());
+		diagram.addMouseWheelListener(new MouseWheelListener() {
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				int notches = e.getWheelRotation();
+				int h = diagram.getHeight();
+				if (notches > 0) {
+					// diagram.hstep+=9.5;
+					diagram.setSize(diagram.getWidth(), h + 10);
+					diagram.setPreferredSize(new Dimension(diagram.getWidth(),
+							h));
+					diagram.revalidate();
+					diagram.once = false;
+					diagram.repaint();
+					scrollPane.revalidate();
+
+				} else if (notches < 0) {
+					diagram.setSize(diagram.getWidth(), h - 10);
+					// diagram.hstep-=h*0.95;
+					diagram.setPreferredSize(new Dimension(diagram.getWidth(),
+							h));
+					diagram.revalidate();
+					diagram.once = false;
+					diagram.repaint();
+					scrollPane.revalidate();
+				}
+			}
+		});
+		diagram.setAutoscrolls(true);
+		diagram.setPreferredSize(new Dimension(3000, 100));
+		diagram.setBackground(Color.pink);
+
+		diagram.file = new File("./wav/artificial/easy/225Hz.wav");
 		
-		scrollPane = new JScrollPane();
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		contentPane.add(scrollPane);
+		scrollPane = new JScrollPane(diagram);
+		scrollPane.setPreferredSize(new Dimension(3002, 100));
+		scrollPane.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 0;
+		scrollPane.revalidate();
+		contentPane.add(scrollPane, gbc_scrollPane);
+
+		JPanel panelBottom = new JPanel();
+		panelBottom.setPreferredSize(new Dimension(10, 100));
+		panelBottom.setSize(new Dimension(0, 100));
+		panelBottom.setMinimumSize(new Dimension(10, 200));
+		panelBottom.setBackground(Color.LIGHT_GRAY);
+		GridBagConstraints gbc_panelBottom = new GridBagConstraints();
+		gbc_panelBottom.fill = GridBagConstraints.BOTH;
+		gbc_panelBottom.gridx = 0;
+		gbc_panelBottom.gridy = 1;
+		contentPane.add(panelBottom, gbc_panelBottom);
+		GridBagLayout gbl_panelBottom = new GridBagLayout();
+		gbl_panelBottom.columnWidths = new int[] { 100, 232, 0, 0, 0 };
+		gbl_panelBottom.rowHeights = new int[] { 20, 0 };
+		gbl_panelBottom.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0,
+				Double.MIN_VALUE };
+		gbl_panelBottom.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
+		panelBottom.setLayout(gbl_panelBottom);
+
+		JLabel labelInputFile = new JLabel("Input file:");
+		GridBagConstraints gbc_labelInputFile = new GridBagConstraints();
+		gbc_labelInputFile.anchor = GridBagConstraints.WEST;
+		gbc_labelInputFile.insets = new Insets(0, 0, 0, 5);
+		gbc_labelInputFile.gridx = 0;
+		gbc_labelInputFile.gridy = 0;
+		panelBottom.add(labelInputFile, gbc_labelInputFile);
+
+		textFieldInputFile = new JTextField();
+		textFieldInputFile.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				int returnVal = fc.showOpenDialog(MainWindow.this);
+
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fc.getSelectedFile();
+					diagram.file = file;
+					textFieldInputFile.setText(file.getAbsolutePath());
+				} else
+					textFieldInputFile.setText("");
+			}
+		});
+		textFieldInputFile.setSize(new Dimension(100, 0));
+		textFieldInputFile.setMinimumSize(new Dimension(60, 20));
+		textFieldInputFile.setPreferredSize(new Dimension(60, 20));
+		textFieldInputFile.setEnabled(false);
+		textFieldInputFile.setEditable(false);
+		GridBagConstraints gbc_textFieldInputFile = new GridBagConstraints();
+		gbc_textFieldInputFile.insets = new Insets(0, 0, 0, 5);
+		gbc_textFieldInputFile.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFieldInputFile.anchor = GridBagConstraints.NORTH;
+		gbc_textFieldInputFile.gridx = 1;
+		gbc_textFieldInputFile.gridy = 0;
+		panelBottom.add(textFieldInputFile, gbc_textFieldInputFile);
+		textFieldInputFile.setColumns(10);
 		
+		btnGenerateWave = new JButton("GENERATE WAVE");
+		btnGenerateWave.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				diagram.repaint();
+			}
+		});
+		GridBagConstraints gbc_btnGenerateWave = new GridBagConstraints();
+		gbc_btnGenerateWave.insets = new Insets(0, 0, 0, 5);
+		gbc_btnGenerateWave.gridx = 2;
+		gbc_btnGenerateWave.gridy = 0;
+		panelBottom.add(btnGenerateWave, gbc_btnGenerateWave);
+
 		panel = new JPanel();
 		panel.setMaximumSize(new Dimension(32767, 50));
 		panel.setPreferredSize(new Dimension(1000, 50));
 		panel.setBackground(Color.LIGHT_GRAY);
 		panel.setMinimumSize(new Dimension(1000, 50));
 		panel.setBounds(new Rectangle(0, 0, 1000, 50));
-//		scrollPane.setRowHeaderView(panel);
-//		scrollPane.add(panel);
+		// scrollPane.setRowHeaderView(panel);
+		// scrollPane.add(panel);
 		panel.setLayout(null);
+
+		// scrollPane.add(diagram);
+		// contentPane.add(diagram);
 	}
 
 	public JPanel getPanel() {
 		return panel;
 	}
-	
+
 	public void setPanel(JPanel panel) {
 		this.panel = panel;
 	}
-	
+
 	public JScrollPane getScrollPane() {
 		return scrollPane;
 	}
