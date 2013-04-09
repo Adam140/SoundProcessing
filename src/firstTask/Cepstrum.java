db756d2db9c4bae613c4536ca3d80414e3f9173c
Quee <zergx1@gmail.com>
2013-04-09 20:38:31 +0200

    cepstrum

--- src/firstTask/Cepstrum.java
+++ src/firstTask/Cepstrum.java
@@ -2,6 +2,7 @@ package firstTask;
 
 
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -85,8 +86,10 @@ public class Cepstrum {
         
         Complex[] s = new Complex[N];
         Vector< double[] > vec = new Vector< double[]>();
+        // 0 - freq , 1- position
+        Vector< double[] > detected_freq = new Vector< double[]>();
+        int zero_index = 0;
         
-       
         while( decoder.readSamples( samples ) > 0 )
         {
         	
@@ -98,6 +101,7 @@ public class Cepstrum {
                 	y[i] = samples[i];
                 	x[i] = i ;
                 	s[i] = new Complex(samples[i],0);
+                	//end[i] = 0;
                 }
                 s = FFT.fft1D(s);
                 //s = FFT.ifft1D(s);
@@ -109,69 +113,94 @@ public class Cepstrum {
                 	s[i] = new Complex(temp[i],0);
                 }
                 s = FFT.ifft1D(s);
+                zero_index = 0;
                 for(int i=0;i<N;i++)
                 {
                 	
-                	temp[i] = Math.abs(s[i].getReal());
-                	end[i] += temp[i];
-
+                	temp[i] = s[i].getReal();
+                	if(zero_index == 0 && temp[i] < 0)
+                		zero_index = i;
+                	end[i] += Math.abs(temp[i]);
                 }
                 
-              
                 
-                vec.add( temp );
+              
+                //break;
+                //vec.add( temp );
                 
         }
  
         boolean going_down = true;
         double max_value = 0;
         int max_index = 0;
-        int left_min_index = 0;
+        int going_down_index = 0;
+        
+        boolean right_going_down = true;
+        int right_going_down_index = 0;
         
         double precision = 0.1;
 //        for(int i=0;i<N;i++)
-//        	end[i] = end[i]/N;
+//        	end[i] = end[i]/N * 10;
         	
         
+//
 
-        for(int i=1;i<N/2;i++)
+        for(int i=zero_index;i<N/2;i++)
         {
-        	if(going_down && end[i] - end[i-1] < precision)
-        		going_down = true;
-        	else
-        		going_down = false;
+//        	if(going_down && end[i] - end[i-1] < precision)
+//        		going_down = true;
+//        	else
+//        	{
+//        		going_down = false;
+//        		going_down_index = i;
+//        	}
         	
-        	if(!going_down)
-        	{
+//        	
+//        	if(!going_down)
+//        	{
         		if(end[i] > max_value)
         		{
         			max_value = end[i];
         			max_index = i;
         		}
-        	}
+        	//}
         		
         }
+        
 //      spectrummax = float(spectrumlen) / pos
 //      return float(spectrummax * FRAMERATE) / frames
         double freq = 0;
         max_value =  (double)N / max_index;
         freq = (max_value * framerate)/N;
         System.out.println("Detected freq: "+freq+"Hz");
-        
+        System.out.println("Position: "+max_index);
+        System.out.println("value: "+max_value);
+        System.out.println("value: "+end[max_index]);
+
+        System.out.println("zero index: "+zero_index);
+
+        System.out.println("Left going down: "+going_down_index);
+        System.out.println("Right going down: "+right_going_down_index);
 
         
+       x = TrimTable(zero_index,N/2, x);
+       end =  TrimTable(zero_index,N/2, end);
+
 		Plot2DPanel plot = new Plot2DPanel();
 		plot.addLegend("SOUTH");
 
 		// add a line plot to the PlotPanel
 		//plot.addLinePlot("Normal", x, y);
-		plot.addLinePlot("Spec", x, end);
-		//plot.addScatterPlot("Spec", temp, y);
-
+		plot.addLinePlot("Cepstrum", x, end);
+		//plot.addScatterPlot("Point", (double[]])max_index);
+		//plot.addLinePlot("Point",Color.RED, new double[] { max_index,max_index }, new double[] { getMaxValue(end),getMinValue(end) });
 		JFrame frame = new JFrame("a plot panel");
 		frame.setSize(600, 600);
 		frame.setContentPane(plot);
 		frame.setVisible(true);
+		
+        
+
 		return freq;
 
 	}
@@ -193,5 +222,45 @@ public class Cepstrum {
 			return (lower + upper) / 2.0;
 	    }	
 	}
+	
+	public double[] TrimTable(int ind,int to,double[] tab)
+	{
+		int size = tab.length - ind-(tab.length-to);
+		double[] tmp = new double[size];
+		for(int i=0;i<size;i++)
+		{
+			tmp[i] = tab[i+ind];
+		}
+		return tmp;
+	}
+	
+	public double ComputeFrequency(int max_index,int N,int framerate)
+	{
+        double freq = 0;
+        double max_value =  (double)N / max_index;
+        freq = (max_value * framerate)/N;
+        return freq;
+	}
+	
+	public static double getMaxValue(double[] numbers){  
+	    double maxValue = numbers[0];  
+	    for(int i=1;i<numbers.length;i++){  
+	        if(numbers[i] > maxValue){  
+	            maxValue = numbers[i];  
+	        }  
+	    }  
+	    return maxValue;  
+	}  
+	  
+	//Find minimum (lowest) value in array using loop  
+	public static double getMinValue(double[] numbers){  
+	    double minValue = numbers[0];  
+	    for(int i=1;i<numbers.length;i++){  
+	        if(numbers[i] < minValue){  
+	            minValue = numbers[i];  
+	        }  
+	    }  
+	    return minValue;  
+	} 
 
 }
